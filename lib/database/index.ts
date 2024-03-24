cec55d83ea789e92b14c9dd49ac126b5a0d0ee0d
Ryan Raushan <raushankrpnhs2000@gmail.com>
2024-03-24 23:28:01 +0530

    Update database models to use "evently" db name and correct User model name case
    - Change MONGODB\_URI db name to "evently"
    - Correct case of User model name in model definition
    
    This commit updates the database models to use the correct db name "evently" and fixes the case of the User model name in the model definition.

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI;
+
 let cached = (global as any).mongoose || { conn: null, promise: null };
 
 export const connectToDatabase = async () => {
@@ -11,10 +12,11 @@ export const connectToDatabase = async () => {
   cached.promise =
     cached.promise ||
     mongoose.connect(MONGODB_URI, {
-      dbName: "Ananta",
+      dbName: "evently",
       bufferCommands: false,
     });
 
   cached.conn = await cached.promise;
+
   return cached.conn;
 };
